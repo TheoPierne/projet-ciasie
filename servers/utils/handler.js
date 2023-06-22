e7f7f11c3d6bde913343f007601826d92dc584e1
TheoPierne <theo.pierne2@gmail.com>
2023-06-23 00:46:58 +0200

    Execution de la fonction benchmark en asynchrone

--- servers/utils/handler.js
+++ servers/utils/handler.js
@@ -1,7 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.createHandler = void 0;
-const handler_1 = require("../../node_modules/graphql-http/lib/handler");
+
+const process = require('node:process');
+const handler_1 = require('../../node_modules/graphql-http/lib/handler');
 /**
  * Create a GraphQL over HTTP spec compliant request handler for
  * the express framework.
@@ -21,40 +23,38 @@ const handler_1 = require("../../node_modules/graphql-http/lib/handler");
  * @category Server/express
  */
 function createHandler(options, benchmark) {
-    const handle = (0, handler_1.createHandler)(options);
-    return async function requestListener(req, res) {
-        let startTime = process.hrtime();
-        try {
-            const [body, init] = await handle({
-                url: req.url,
-                method: req.method,
-                headers: req.headers,
-                body: () => {
-                    if (req.body) {
-                        // in case express has a body parser
-                        return req.body;
-                    }
-                    return new Promise((resolve) => {
-                        let body = '';
-                        req.on('data', (chunk) => (body += chunk));
-                        req.on('end', () => resolve(body));
-                    });
-                },
-                raw: req,
-                context: { res },
-            });
-            let jsonBody = JSON.parse(body);
-            jsonBody.benchmark = benchmark(startTime);
-            res.writeHead(init.status, init.statusText, init.headers).end(JSON.stringify(jsonBody));
-            jsonBody = null;
-        }
-        catch (err) {
-            // The handler shouldnt throw errors.
-            // If you wish to handle them differently, consider implementing your own request handler.
-            console.error('Internal error occurred during request handling. ' +
-                'Please check your implementation.', err);
-            res.writeHead(500).end();
-        }
-    };
+  const handle = (0, handler_1.createHandler)(options);
+  return async function requestListener(req, res) {
+    let startTime = process.hrtime();
+    try {
+      const [body, init] = await handle({
+        url: req.url,
+        method: req.method,
+        headers: req.headers,
+        body: () => {
+          if (req.body) {
+            // in case express has a body parser
+            return req.body;
+          }
+          return new Promise(resolve => {
+            let dataBody = '';
+            req.on('data', chunk => (dataBody += chunk));
+            req.on('end', () => resolve(dataBody));
+          });
+        },
+        raw: req,
+        context: { res },
+      });
+      let jsonBody = JSON.parse(body);
+      jsonBody.benchmark = await benchmark(startTime);
+      res.writeHead(init.status, init.statusText, init.headers).end(JSON.stringify(jsonBody));
+      jsonBody = null;
+    } catch (err) {
+      // The handler shouldnt throw errors.
+      // If you wish to handle them differently, consider implementing your own request handler.
+      console.error('Internal error occurred during request handling. ' + 'Please check your implementation.', err);
+      res.writeHead(500).end();
+    }
+  };
 }
 exports.createHandler = createHandler;
