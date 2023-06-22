22cbc8ca7ed2df03d5c3dbcf26df293df34b315d
TheoPierne <theo.pierne2@gmail.com>
2023-06-22 11:56:58 +0200

    Correction erreur CORS

--- servers/graphql/server.js
+++ servers/graphql/server.js
@@ -5,8 +5,9 @@ const process = require('node:process');
 
 require('dotenv').config({ path: path.resolve(process.cwd(), '.env') });
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT_GRAPHQL } = process.env;
 
+const cors = require('cors');
 const express = require('express');
 const { createHandler } = require('graphql-http/lib/use/express');
 const graphqlResolvers = require('./resolvers');
@@ -15,12 +16,17 @@ const mongoDBConnect = require('../utils/connectDB');
 
 const app = express();
 
-app.all('/graphql', createHandler({
-  schema: graphqlSchema,
-  rootValue: graphqlResolvers,
-  graphiql: true,
-}));
+app.use(cors());
+
+app.all(
+  '/graphql',
+  createHandler({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true,
+  }),
+);
 
 mongoDBConnect()
-  .then(() => app.listen(SERVER_PORT, () => console.log(`API GRAPHQL on port: ${SERVER_PORT}`)))
+  .then(() => app.listen(SERVER_PORT_GRAPHQL, () => console.log(`API GRAPHQL on port: ${SERVER_PORT_GRAPHQL}`)))
   .catch(console.error);
