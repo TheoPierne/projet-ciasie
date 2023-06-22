21923c22137323cfb3613008d84ba5c23a3f03bc
winvdaking <pro.dorianlopez@gmail.com>
2023-06-22 14:12:31 +0200

    chore

--- servers/rest/server.js
+++ servers/rest/server.js
@@ -35,22 +35,13 @@ app.get('/users/:id/messages', async (req, res) =>
   res.send(getBenchmark(process.hrtime(), await Messages.find({ author: req.params.id }).exec())),
 );
 
-<<<<<<< HEAD
-app.get('/users', async (req, res) => res.send(getBenchmark(process.hrtime(), await Users.find({}, '_id').exec())));
-=======
 app.get('/users', async (req, res) => res.send(getBenchmark(process.hrtime(), await Users.find({}).exec())));
->>>>>>> aff9c4f58322eb27a4ebd60e197179e888eda0be
 
 app.get('/', (req, res) => res.send(getBenchmark(process.hrtime(), null)));
 
 mongoDBConnect()
-<<<<<<< HEAD
-    .then(() => app.listen(SERVER_PORT, () => console.log(`REST API on port: ${SERVER_PORT}`)))
-    .catch(console.error);
-=======
   .then(() => app.listen(SERVER_PORT_REST, () => console.log(`REST API on port: ${SERVER_PORT_REST}`)))
   .catch(console.error);
->>>>>>> aff9c4f58322eb27a4ebd60e197179e888eda0be
 
 /**
  * Dze
@@ -59,17 +50,6 @@ mongoDBConnect()
  * @returns {Object} La requête demandée + le benchmark
  */
 function getBenchmark(startTime, data) {
-<<<<<<< HEAD
-    return {
-        data: data,
-        benchmark: {
-            RAMALLOWED: process.memoryUsage.rss() + ' octets',
-            RAMPOSSIBILITY: process.memoryUsage(),
-            CPU: process.cpuUsage(),
-            TIME: process.hrtime(startTime)[0] + 's & ' + process.hrtime(startTime)[1] + 'ms'
-        },
-    }
-=======
   return {
     data,
     benchmark: {
@@ -79,5 +59,4 @@ function getBenchmark(startTime, data) {
       TIME: process.hrtime(startTime),
     },
   };
->>>>>>> aff9c4f58322eb27a4ebd60e197179e888eda0be
 }
