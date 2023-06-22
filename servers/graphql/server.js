c93d0d64751e4717995ce5d3fdfe78d6064d644d
TheoPierne <theo.pierne2@gmail.com>
2023-06-23 00:46:19 +0200

    Modification de la récupération du % CPU

--- servers/graphql/server.js
+++ servers/graphql/server.js
@@ -2,7 +2,6 @@
 
 const path = require('node:path');
 const process = require('node:process');
-const os = require('node:os');
 
 require('dotenv').config({ path: path.resolve(process.cwd(), '.env') });
 
@@ -10,6 +9,8 @@ const { SERVER_PORT_GRAPHQL } = process.env;
 
 const cors = require('cors');
 const express = require('express');
+const pidusage = require('pidusage');
+
 const graphqlResolvers = require('./resolvers');
 const graphqlSchema = require('./schema');
 const mongoDBConnect = require('../utils/connectDB');
@@ -19,51 +20,31 @@ const app = express();
 
 app.use(cors());
 
-app.all('/graphql', (req, res) => createHandler({
-  schema: graphqlSchema,
-  rootValue: graphqlResolvers,
-  graphiql: true,
-}, getBenchmark)(req, res));
+app.all('/graphql', (req, res) =>
+  createHandler(
+    {
+      schema: graphqlSchema,
+      rootValue: graphqlResolvers,
+      graphiql: true,
+    },
+    getBenchmark,
+  )(req, res),
+);
 
 mongoDBConnect()
   .then(() => app.listen(SERVER_PORT_GRAPHQL, () => console.log(`API GRAPHQL on port: ${SERVER_PORT_GRAPHQL}`)))
   .catch(console.error);
 
-
 /**
-* 
-* @param {number} startTime 
-* @param {} data 
-* @returns La requête demandée + le benchmark
-*/
-function getBenchmark(startTime) {
+ * Retourne un objet contenant les données du benchmark
+ * @param {number} startTime Le temps au début de la requête Mongo
+ * @returns {Promise<Object>} La requête demandée + le benchmark
+ */
+async function getBenchmark(startTime) {
   return {
     RAMALLOWED: process.memoryUsage.rss(),
     RAMPOSSIBILITY: process.memoryUsage(),
-    CPU: getCpuUsage(),
-    TIME: process.hrtime(startTime)
-  }
-}
-
-function getCpuUsage() {
-  const cpuInfo = os.cpus();
-  const numCores = cpuInfo.length;
-
-  let totalIdle = 0;
-  let totalTick = 0;
-
-  for (let i = 0; i < numCores; i++) {
-    const cpu = cpuInfo[i];
-
-    for (let type in cpu.times) {
-      totalTick += cpu.times[type];
-    }
-
-    totalIdle += cpu.times.idle;
-  }
-
-  const idle = totalIdle / numCores;
-  const total = totalTick / numCores;
-
-  return (100 - ~~(100 * idle / total));
+    CPU: (await pidusage(process.pid)).cpu,
+    TIME: process.hrtime(startTime),
+  };
 }
--- servers/rest/server.js
+++ servers/rest/server.js
@@ -2,13 +2,14 @@
 
 const path = require('node:path');
 const process = require('node:process');
-const os = require('node:os');
 
 require('dotenv').config({ path: path.resolve(process.cwd(), '.env') });
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
+const pidusage = require('pidusage');
+
 const Messages = require('../models/message');
 const Users = require('../models/user');
 const mongoDBConnect = require('../utils/connectDB');
@@ -20,25 +21,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/messages', async (req, res) => res.send(getBenchmark(process.hrtime(), await Messages.find({}).exec())));
-
-app.get('/messages/:id', async (req, res) =>
-  res.send(getBenchmark(process.hrtime(), await Messages.findById(req.params.id).exec())),
+app.get('/messages', async (req, res) =>
+  res.send(await getBenchmark(process.hrtime(), await Messages.find({}).exec())),
 );
 
-app.post('/messages', async (req, res) => res.send(getBenchmark(process.hrtime(), await Messages.create(req.body))));
+app.get('/messages/:id', async (req, res) =>
+  res.send(await getBenchmark(process.hrtime(), await Messages.findById(req.params.id).exec())),
+);
+
+app.post('/messages', async (req, res) =>
+  res.send(await getBenchmark(process.hrtime(), await Messages.create(req.body))),
+);
 
 app.get('/users/:id', async (req, res) =>
-  res.send(getBenchmark(process.hrtime(), await Users.findById(req.params.id).exec())),
+  res.send(await getBenchmark(process.hrtime(), await Users.findById(req.params.id).exec())),
 );
 
 app.get('/users/:id/messages', async (req, res) =>
-  res.send(getBenchmark(process.hrtime(), await Messages.find({ author: req.params.id }).exec())),
+  res.send(await getBenchmark(process.hrtime(), await Messages.find({ author: req.params.id }).exec())),
 );
 
-app.get('/users', async (req, res) => res.send(getBenchmark(process.hrtime(), await Users.find({}).exec())));
+app.get('/users', async (req, res) => res.send(await getBenchmark(process.hrtime(), await Users.find({}).exec())));
 
-app.get('/', (req, res) => res.send(getBenchmark(process.hrtime(), null)));
+app.get('/', async (req, res) => res.send(await getBenchmark(process.hrtime(), null)));
 
 mongoDBConnect()
   .then(() => app.listen(SERVER_PORT_REST, () => console.log(`REST API on port: ${SERVER_PORT_REST}`)))
@@ -50,37 +55,14 @@ mongoDBConnect()
  * @param {Messages} data dd
  * @returns {Object} La requête demandée + le benchmark
  */
-function getBenchmark(startTime, data) {
+async function getBenchmark(startTime, data) {
   return {
     data,
     benchmark: {
       RAMALLOWED: process.memoryUsage.rss(),
       RAMPOSSIBILITY: process.memoryUsage(),
-      CPU: getCpuUsage(),
+      CPU: (await pidusage(process.pid)).cpu,
       TIME: process.hrtime(startTime),
     },
   };
-}
-
-function getCpuUsage() {
-  const cpuInfo = os.cpus();
-  const numCores = cpuInfo.length;
-
-  let totalIdle = 0;
-  let totalTick = 0;
-
-  for (let i = 0; i < numCores; i++) {
-    const cpu = cpuInfo[i];
-
-    for (let type in cpu.times) {
-      totalTick += cpu.times[type];
-    }
-
-    totalIdle += cpu.times.idle;
-  }
-
-  const idle = totalIdle / numCores;
-  const total = totalTick / numCores;
-
-  return (100 - ~~(100 * idle / total));
 }
