badf36687e26a20eef6db04a036eed4309fa0e56
TheoPierne <theo.pierne2@gmail.com>
2023-06-22 11:58:40 +0200

    Modification routes

--- servers/rest/server.js
+++ servers/rest/server.js
@@ -4,53 +4,59 @@ const path = require('node:path');
 const process = require('node:process');
 
 require('dotenv').config({ path: path.resolve(process.cwd(), '.env') });
-const mongoDBConnect = require('../utils/connectDB');
-const Messages = require('../models/message');
-const Users = require('../models/user');
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
+const Messages = require('../models/message');
+const Users = require('../models/user');
+const mongoDBConnect = require('../utils/connectDB');
 
 const app = express();
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT_REST } = process.env;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/messages', async (req, res) => res.send(getBenchmark(process.hrtime(), await Messages.find({}, '_id').exec())));
+app.get('/messages', async (req, res) => res.send(getBenchmark(process.hrtime(), await Messages.find({}).exec())));
 
-app.get('/messages/:id', async (req, res) => res.send(getBenchmark(process.hrtime(), await Messages.findById(req.params.id).exec())));
+app.get('/messages/:id', async (req, res) =>
+  res.send(getBenchmark(process.hrtime(), await Messages.findById(req.params.id).exec())),
+);
 
-app.post('/message', async (req, res) => res.send(getBenchmark(process.hrtime(), await Messages.create(req.body))));
+app.post('/messages', async (req, res) => res.send(getBenchmark(process.hrtime(), await Messages.create(req.body))));
 
-app.get('/user/:id', async (req, res) => res.send(getBenchmark(process.hrtime(), await Users.findById(req.params.id).exec())));
+app.get('/users/:id', async (req, res) =>
+  res.send(getBenchmark(process.hrtime(), await Users.findById(req.params.id).exec())),
+);
 
-app.get('/user/:id/messages', async (req, res) => res.send(getBenchmark(process.hrtime(), await Messages.find({ 'author': req.params.id }).exec())));
+app.get('/users/:id/messages', async (req, res) =>
+  res.send(getBenchmark(process.hrtime(), await Messages.find({ author: req.params.id }).exec())),
+);
 
-app.get('/user', async (req, res) => res.send(getBenchmark(process.hrtime(), await Users.find({}, '_id').exec())));
+app.get('/users', async (req, res) => res.send(getBenchmark(process.hrtime(), await Users.find({}).exec())));
 
 app.get('/', (req, res) => res.send(getBenchmark(process.hrtime(), null)));
 
 mongoDBConnect()
-  .then(() => app.listen(SERVER_PORT, () => console.log(`REST API on port: ${SERVER_PORT}`)))
+  .then(() => app.listen(SERVER_PORT_REST, () => console.log(`REST API on port: ${SERVER_PORT_REST}`)))
   .catch(console.error);
 
 /**
- * 
- * @param {Int} startTime 
- * @param {Messaeges} data 
- * @returns La requête demandée + le benchmark
+ * Dze
+ * @param {number} startTime dd
+ * @param {Messages} data dd
+ * @returns {Object} La requête demandée + le benchmark
  */
 function getBenchmark(startTime, data) {
   return {
-    data: data,
+    data,
     benchmark: {
-      RAMALLOWED: process.memoryUsage.rss() + ' octets',
+      RAMALLOWED: process.memoryUsage.rss(),
       RAMPOSSIBILITY: process.memoryUsage(),
       CPU: process.cpuUsage(),
-      TIME: process.hrtime(startTime)[0] + 's & ' + process.hrtime(startTime)[1] + 'ms'
+      TIME: process.hrtime(startTime),
     },
-  }
+  };
 }
