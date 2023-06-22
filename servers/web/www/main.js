aff9c4f58322eb27a4ebd60e197179e888eda0be
TheoPierne <theo.pierne2@gmail.com>
2023-06-22 11:58:58 +0200

    Correction fichier main.js

--- servers/web/www/main.js
+++ servers/web/www/main.js
@@ -4,45 +4,27 @@
 const routeApiRest = [
   {
     method: 'get',
-    url: '/user',
-    vRam: 18524,
-    vProc: 2050,
-    vTime: 2064,
+    url: '/users',
   },
   {
     method: 'get',
     url: '/user/d2fa789fab66eb6d2b2a9a68',
-    vRam: 16084,
-    vProc: 1800,
-    vTime: 1478,
   },
   {
     method: 'get',
     url: '/messages',
-    vRam: 18524,
-    vProc: 2050,
-    vTime: 2064,
   },
   {
     method: 'get',
     url: '/user/d2fa789fab66eb6d2b2a9a68/messages',
-    vRam: 16084,
-    vProc: 1800,
-    vTime: 1478,
   },
   {
     method: 'get',
-    url: '/messages/d2fa789fab66eb6d2b2a9a68',
-    vRam: 18524,
-    vProc: 2050,
-    vTime: 2064,
+    url: '/messages/3f06f813b84acd6c8b36bbd9',
   },
   {
     method: 'post',
     url: '/message',
-    vRam: 16084,
-    vProc: 1800,
-    vTime: 1478,
   },
 ];
 
@@ -52,60 +34,81 @@ const routeGraphQL = [
     method: 'post',
     url: '/graphql',
     query: '{users{_id,username,phoneNumber,email,avatar,createdAt}}',
-    vRam: 16084,
-    vProc: 1800,
-    vTime: 1478,
   },
   {
     method: 'post',
     url: '/graphql',
     query: '{user(id: "d2fa789fab66eb6d2b2a9a68") {username,phoneNumber,email,avatar,createdAt,}}',
-    vRam: 18524,
-    vProc: 2050,
-    vTime: 2064,
   },
   {
     method: 'post',
     url: '/graphql',
     query: '{messages{_id,content,createdAt}}',
-    vRam: 16084,
-    vProc: 1800,
-    vTime: 1478,
   },
   {
     method: 'post',
     url: '/graphql',
     // eslint-disable-next-line no-inline-comments,no-warning-comments
     query: "Voir les messages d'un user", // TODO
-    vRam: 18524,
-    vProc: 2050,
-    vTime: 2064,
   },
   {
     method: 'post',
     url: '/graphql',
     query: '{message(id:"3f06f813b84acd6c8b36bbd9"){_id,content,createdAt}}',
-    vRam: 16084,
-    vProc: 1800,
-    vTime: 1478,
   },
   {
     method: 'post',
     url: '/graphql',
     // eslint-disable-next-line no-inline-comments,no-warning-comments
     query: 'Ajouter un message', // TODO
-    vRam: 18524,
-    vProc: 2050,
-    vTime: 2064,
   },
 ];
 
 // fetch('http://localhost/').then(r => r.json()).then(r => console.log(r));
 
+// TODO: fetch data from server Rest API
+async function makeRestApiRequest() {
+  const startTime = Date.now();
+  const restResponse = await fetch(`http://localhost:3030${routeApiRest[selectQuery.value].url}`, {
+    method: `${routeApiRest[selectQuery.value].method}`,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Apikey DONOTSENDAPIKEYS',
+    },
+  });
+
+  const requestDuration = Date.now() - startTime;
+  const response = await restResponse.json();
+  const requestSize = +restResponse.headers.get('Content-Length');
+
+  return { duration: requestDuration, size: requestSize, response };
+}
+
+// TODO: fetch data from server GraphQL API
+async function makeGraphQLRequest() {
+  const data = JSON.stringify({ query: routeGraphQL[selectQuery.value].query });
+  const startTime = Date.now();
+  const gqlResponse = await fetch(`http://localhost:3000${routeGraphQL[selectQuery.value].url}`, {
+    method: `${routeGraphQL[selectQuery.value].method}`,
+    body: data,
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length,
+      Authorization: 'Apikey DONOTSENDAPIKEYS',
+    },
+  });
+
+  const requestDuration = Date.now() - startTime;
+  const response = await gqlResponse.json();
+  const requestSize = JSON.stringify(response).length;
+
+  return { duration: requestDuration, size: requestSize, response };
+}
+
 const reloadBtn = document.querySelector('#reload');
 const selectQuery = document.querySelector('#select-query');
 
-reloadBtn.addEventListener('click', () => {
+reloadBtn.addEventListener('click', async () => {
   console.log(routeGraphQL[selectQuery.value]);
   console.log(routeApiRest[selectQuery.value]);
 
@@ -122,67 +125,22 @@ reloadBtn.addEventListener('click', () => {
 
   */
 
-  objRamChart.data.datasets[0].data[0] = 0;
-  objRamChart.data.datasets[0].data[1] = 0;
+  const graphQlData = await makeGraphQLRequest();
+  const restApiData = await makeRestApiRequest();
 
-  objProcChart.data.datasets[0].data[0] = 0;
-  objProcChart.data.datasets[0].data[1] = 0;
+  console.log(graphQlData, restApiData);
 
-  objTimeChart.data.datasets[0].data[0] = 0;
-  objTimeChart.data.datasets[0].data[1] = 0;
+  objRamChart.data.datasets[0].data[0] = graphQlData.duration;
+  objProcChart.data.datasets[0].data[0] = 0; // gqlData.proc;
+  objTimeChart.data.datasets[0].data[0] = graphQlData.size / 1000;
+
+  objRamChart.data.datasets[0].data[1] = restApiData.duration;
+  objProcChart.data.datasets[0].data[1] = 0; // restData.proc;
+  objTimeChart.data.datasets[0].data[1] = restApiData.size / 1000;
 
   objRamChart.update();
   objProcChart.update();
   objTimeChart.update();
-
-  // TODO: fetch data from server Rest API
-  // eslint-disable-next-line func-names
-  (async function () {
-    let restResponse = await fetch(`http://localhost:3000${routeApiRest[selectQuery.value].url}`, {
-      method: `${routeApiRest[selectQuery.value].method}`,
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Apikey DONOTSENDAPIKEYS',
-      },
-    });
-
-    let restData = await restResponse.json();
-    console.log(restData);
-
-    objRamChart.data.datasets[0].data[1] = 0; // restData.ram;
-    objProcChart.data.datasets[0].data[1] = 0; // restData.proc;
-    objTimeChart.data.datasets[0].data[1] = 0; // restData.time;
-
-    objRamChart.update();
-    objProcChart.update();
-    objTimeChart.update();
-  })();
-
-  // TODO: fetch data from server GraphQL API
-  // eslint-disable-next-line func-names
-  (async function () {
-    let data = JSON.stringify({ query: routeGraphQL[selectQuery.value].query });
-    let gqlResponse = await fetch(`http://localhost:3000${routeGraphQL[selectQuery.value].url}`, {
-      method: `${routeGraphQL[selectQuery.value].method}`,
-      body: data,
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': data.length,
-        Authorization: 'Apikey DONOTSENDAPIKEYS',
-      },
-    });
-
-    let gqlData = await gqlResponse.json();
-    console.log(gqlData);
-
-    objRamChart.data.datasets[0].data[0] = 0; // gqlData.ram;
-    objProcChart.data.datasets[0].data[0] = 0; // gqlData.proc;
-    objTimeChart.data.datasets[0].data[0] = 0; // gqlData.time;
-
-    objRamChart.update();
-    objProcChart.update();
-    objTimeChart.update();
-  })();
 });
 
 const ramChart = document.getElementById('ram');
@@ -190,13 +148,13 @@ const procChart = document.getElementById('proc');
 const timeChart = document.getElementById('time');
 
 // eslint-disable-next-line no-new,no-undef
-let objRamChart = new Chart(ramChart, {
+const objRamChart = new Chart(ramChart, {
   type: 'bar',
   data: {
     labels: ['GraphQL', 'REST-Api'],
     datasets: [
       {
-        label: 'RAM (k/o)',
+        label: 'RAM (Ko)',
         data: [0, 0],
         borderWidth: 1,
         backgroundColor: ['rgba(255, 99, 132)', 'rgba(54, 162, 235)'],
@@ -212,13 +170,13 @@ let objRamChart = new Chart(ramChart, {
   },
 });
 // eslint-disable-next-line no-new,no-undef
-let objProcChart = new Chart(procChart, {
+const objProcChart = new Chart(procChart, {
   type: 'bar',
   data: {
     labels: ['GraphQL', 'REST-Api'],
     datasets: [
       {
-        label: 'PROC (k/o)',
+        label: 'PROC (%)',
         data: [0, 0],
         borderWidth: 1,
         backgroundColor: ['rgba(255, 99, 132)', 'rgba(54, 162, 235)'],
@@ -235,13 +193,13 @@ let objProcChart = new Chart(procChart, {
 });
 
 // eslint-disable-next-line no-new,no-undef
-let objTimeChart = new Chart(timeChart, {
+const objTimeChart = new Chart(timeChart, {
   type: 'bar',
   data: {
     labels: ['GraphQL', 'REST-Api'],
     datasets: [
       {
-        label: 'Time (ms)',
+        label: 'Size (Ko)',
         data: [0, 0],
         borderWidth: 1,
         backgroundColor: ['rgba(255, 99, 132)', 'rgba(54, 162, 235)'],
